5c443d9ee4253c0cd3f6f9a24dd5ac8795bc6abc
Joao Victor Rodrigues Hilario <jvrodrigueshilario@gmail.com>
2020-04-20 17:17:02 -0300

    Updates

---
+++ precache-manifest.b3cf61e82712aefc6f6f1701d74ae198.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "183827390ef4798d24bb2c7de6eadec7",
+    "url": "/myPortfolioSite/index.html"
+  },
+  {
+    "revision": "cb8e045322e7bc5c7da6",
+    "url": "/myPortfolioSite/static/css/2.d788185a.chunk.css"
+  },
+  {
+    "revision": "65c5ae434bd966f3ab5b",
+    "url": "/myPortfolioSite/static/css/main.f5aebe16.chunk.css"
+  },
+  {
+    "revision": "cb8e045322e7bc5c7da6",
+    "url": "/myPortfolioSite/static/js/2.a67e80ca.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/myPortfolioSite/static/js/2.a67e80ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65c5ae434bd966f3ab5b",
+    "url": "/myPortfolioSite/static/js/main.0c715a71.chunk.js"
+  },
+  {
+    "revision": "92dba6e6bad86d258780",
+    "url": "/myPortfolioSite/static/js/runtime-main.5e68b8b6.js"
+  }
+]);
